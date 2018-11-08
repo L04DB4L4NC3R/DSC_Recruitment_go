65976934843fb28e653f12abf5fa4300ab71d98b
angad <angadsharma1016@gmail.com>
2018-11-08 22:51:52 +0530

    DB conn

--- src/webapp/main.go
+++ src/webapp/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
 	"./controller"
 	"github.com/joho/godotenv"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
@@ -15,8 +16,20 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	log.Println(os.Getenv("ADMIN_PASS"))
-	fmt.Println()
+	//log.Println(os.Getenv("ADMIN_PASS"))
+	db := connectDB()
+	defer db.Close()
 	controller.Startup()
 	http.ListenAndServe(":3000", nil)
+}
+
+func connectDB() *sql.DB {
+	db, err := sql.Open(os.Getenv("DBDRIVER"), os.Getenv("DBURL"))
+	if err != nil {
+		log.Println("Unable to connect to DB")
+		return nil
+	} else {
+		log.Println("Connected to DB")
+		return db
+	}
 }
